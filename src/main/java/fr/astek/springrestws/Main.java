0570094ec352c8946d0f9333b926b084b47b878e
Astek SO <odupre@astek.fr>
2015-07-29 17:36:15 +0200

    Preparing deployment on Tomcat

--- src/main/java/fr/astek/springrestws/Main.java
+++ src/main/java/fr/astek/springrestws/Main.java
@@ -2,13 +2,20 @@ package fr.astek.springrestws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * @author odupre
  * @version $Id: $
  */
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
